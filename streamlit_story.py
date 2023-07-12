309608a781e17a5d4e9fdf832d448cc9c479348d
rorysmith85 <32510419+rorysmith85@users.noreply.github.com>
2023-07-12 16:17:13 -0700

    Update streamlit_story.py

--- streamlit_story.py
+++ streamlit_story.py
@@ -472,8 +472,8 @@ def set_page_layout():
         """
         <style>
         #root > div > div > div > div > div > section > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(14) {
-            padding-top: -20px !important;
-            margin-top: -200px !important;
+            padding-bottom: -50px !important;
+            margin-bottom: -50px !important;
         }
         </style>
         """,
